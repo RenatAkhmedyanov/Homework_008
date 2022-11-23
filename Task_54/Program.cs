283fc6741fa559da007482832cca16768f0c25f1
Renat <renatakhmedyanov@gmail.com>
2022-11-23 15:08:26 +0400

    Add Task 54

---
+++ Task_54/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Программа, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+
+Console.Write("Введите число строк таблицы: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число столбцов таблицы: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(m, n);
+Console.WriteLine("Полученный массив:");
+PrintArray(array);
+SortArray(array);
+Console.WriteLine("Отсортированный массив:");
+PrintArray(array);
+
+int[,] GetArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random((int)DateTime.Now.Ticks);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 21);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(String.Format("{0,4}", array[i, j]));
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] > array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+
+        }
+    }
+    return array;
+}
